f6cdcdee7926468c771fc37fa36e188f48476e5e
matheuscarv69 <matheus9126@gmail.com9>
2024-06-17 20:35:03 -0400

    :wrench: feat: adjustment controller and request class

--- src/main/java/com/matheuscarv69/poc_chain_of_responsibility/application/controller/AccessController.java
+++ src/main/java/com/matheuscarv69/poc_chain_of_responsibility/application/controller/AccessController.java
@@ -1,7 +1,26 @@
 package com.matheuscarv69.poc_chain_of_responsibility.application.controller;
 
+import com.matheuscarv69.poc_chain_of_responsibility.application.request.AccessRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/access")
 public class AccessController {
 
+    private static final Logger log = LoggerFactory.getLogger(AccessController.class);
 
+    @PostMapping
+    public ResponseEntity<?> verifyAccess(@RequestBody final AccessRequest request) {
+
+        log.info(request.toString());
+
+        return ResponseEntity.ok().build();
+    }
 
 }
---
+++ src/main/java/com/matheuscarv69/poc_chain_of_responsibility/application/request/AccessRequest.java
@@ -0,0 +1,8 @@
+package com.matheuscarv69.poc_chain_of_responsibility.application.request;
+
+public record AccessRequest(
+
+        String profile,
+        String name
+
+) {}
